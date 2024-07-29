911670bbd59d536783783a915c4a66946414815e
manoj mandal <hackintown99@gmail.com>
2024-07-29 17:39:28 +0530

    Backend Deployed At Render

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     apiBaseUrl:
       process.env.NODE_ENV === "production"
-        ? "https://hackintown-v2-0-server.onrender.com"
+        ? "https://my-portfolio-j1b9.onrender.com"
         : "http://localhost:5000",
   };
   
